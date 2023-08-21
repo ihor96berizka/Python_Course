7e649a3e46d2745d5f6111210c7a39ecfabcde51
LiyaNataliia <s.nataliiaa@gmail.com>
2023-08-21 22:03:35 +0300

    Changed task7

--- lab_1/task7.py
+++ lab_1/task7.py
@@ -1,24 +1,21 @@
 i = 1
 N = 5
 u = 5
-S = 5
 
 while i <= N:
     j = 1
     while j <= i:
-        print(i * 1, end=' ')
-        j = j + 1
+        print(i, end=' ')
+        j += 1
     print()
 
-    i = i + 1
+    i += 1
 
-while u <= S:
+while u > 0:
     j = 1
     while j <= u:
-        print(u * 1, end=" ")
-        j = j + 1
+        print(u, end=" ")
+        j += 1
     print()
 
-    u = u - 1
-    if u == 0:
-        break
+    u -= 1
