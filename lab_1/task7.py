7cdeb642ce01bf6e291fedfe7e2a871698a050f6
LiyaNataliia <136832935+LiyaNataliia@users.noreply.github.com>
2023-08-21 22:25:45 +0300

    Task7 (#43)
    * Completed task7
    
    * Changed task7

---
+++ lab_1/task7.py
@@ -0,0 +1,21 @@
+i = 1
+N = 5
+u = 5
+
+while i <= N:
+    j = 1
+    while j <= i:
+        print(i, end=' ')
+        j += 1
+    print()
+
+    i += 1
+
+while u > 0:
+    j = 1
+    while j <= u:
+        print(u, end=" ")
+        j += 1
+    print()
+
+    u -= 1
