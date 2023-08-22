d63cad923343fde8ab6c59a146d2e746d940f4c9
LiyaNataliia <s.nataliiaa@gmail.com>
2023-08-22 19:12:56 +0300

    Changed task25

--- lab_1/task25.py
+++ lab_1/task25.py
@@ -2,8 +2,10 @@ magic_number = 737
 
 N = int(input("What`s N?"))
 
-while N != magic_number:
-    print("Oops, you did not guess the magic number")
-    break
-else:
-    print("Wow, you guessed the magic number. Now you will earn a lot of dollars")
+while True:
+    if N != magic_number:
+        print("Oops, you did not guess the magic number")
+        N = int(input("What`s N?"))
+    else:
+        print("Wow, you guessed the magic number. Now you will earn a lot of dollars")
+        break
