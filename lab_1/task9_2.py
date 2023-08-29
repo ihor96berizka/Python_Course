c6304b4426c919ba756ca7efddc11e91a18e6591
LiyaNataliia <s.nataliiaa@gmail.com>
2023-08-29 11:59:31 +0300

    Changed

--- lab_1/task9_2.py
+++ lab_1/task9_2.py
@@ -1,8 +1,6 @@
-i = 1
 N = int(input("What`s N"))
 
-for i in range(i, N +1):
-    j = 1
-    for j in range(j, N +1):
+for i in range(1, N +1):
+    for j in range(1, N +1):
         print(i * j, end=' ')
     print()
