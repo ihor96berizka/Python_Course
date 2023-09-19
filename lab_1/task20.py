9a90e972d562219c7d1f7c13a86e7b320f48eb2a
LiyaNataliia <136832935+LiyaNataliia@users.noreply.github.com>
2023-09-19 18:23:34 +0300

    Task 20 (#58)

---
+++ lab_1/task20.py
@@ -0,0 +1,14 @@
+sampleDict = {
+    'class': {
+        'student': {
+            'name': "Mike",
+            'marks': {
+                'physics': 70,
+                'history': 80
+            }
+        }
+    }
+}
+
+physics_mark = sampleDict['class']['student']['marks']['physics']
+print("Physics value is:", physics_mark)
