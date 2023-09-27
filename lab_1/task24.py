d4f0edaee078860e79c1e33827deb01f7a18c1c0
LiyaNataliia <136832935+LiyaNataliia@users.noreply.github.com>
2023-09-27 19:51:20 +0300

    Task 24 (#65)

---
+++ lab_1/task24.py
@@ -0,0 +1,12 @@
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+    }
+
+# Rename the key "city" to "location"
+sample_dict["location"] = sample_dict.pop("city")
+
+# Print the updated dictionary
+print(sample_dict)
