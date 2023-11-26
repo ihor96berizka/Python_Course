d26d1a8f92631aabc6c9f1318cbf9f5e8eebaf74
LiyaNataliia <s.nataliiaa@gmail.com>
2023-11-26 15:20:16 +0200

    Changed task22

--- lab_1/task22.py
+++ lab_1/task22.py
@@ -1,10 +1,19 @@
-sample_dict = {
+def delete_key(key, sample_dict):
+    if key in sample_dict:
+        del sample_dict[key]
+    else:
+        print(key, "don`t found")
+
+
+our_dict = {
     "name": "Kelly",
     "age": 25,
     "salary": 8000,
     "city": "New york"
     }
-del sample_dict["name"]
-del sample_dict["salary"]
 
-print(sample_dict)
+delete_key("name", our_dict)
+delete_key("salary", our_dict)
+delete_key("red", our_dict)
+
+print(our_dict)
