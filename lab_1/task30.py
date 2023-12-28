6564c091dd3011c85d37cd6dd296eae72fb10f1c
LiyaNataliia <s.nataliiaa@gmail.com>
2023-12-28 23:09:28 +0200

    Changed task 30

--- lab_1/task30.py
+++ lab_1/task30.py
@@ -3,9 +3,12 @@ import argparse
 
 # define functions for operation with file
 def read_file(file_name):
-    with open(file_name, 'r') as file:
-        reading = file.read()
-        return reading
+    try:
+        with open(file_name, 'r') as file:
+            reading = file.read()
+            return reading
+    except FileNotFoundError:
+        return None
 
 
 def count_words(file_name):
@@ -17,7 +20,7 @@ def count_words(file_name):
 
 def count_lines(file_name):
     file_reading = read_file(file_name)
-    lines = file_reading.split('/n')
+    lines = file_reading.split('\n')
     line_count = len(lines)
     print(line_count)
 
@@ -31,10 +34,7 @@ def count_some_word(file_name, target_word):
 # define the main function using argparse module
 def main():
     parser = argparse.ArgumentParser(description='Operation with file')
-    # subparsers = parser.add_subparsers(dest='operation', help='Available operations')
 
-    # create the parser for the "file" command
-    # parser_file = subparsers.add_parser('-f', help="open your file")
     parser.add_argument('-f', '--file', help="Only open your file, you need to use this with '-cw'/'-cl'/'-csw'. Example: -f file_name -cw.")
     parser.add_argument('-cw', '--count_words', action='store_true', help="Count words in your file. Example: -f file_name -cw.")
     parser.add_argument('-cl', '--count_lines', action='store_true', help="Count lines in your file. Example: -f file_name -cl.")
@@ -43,7 +43,9 @@ def main():
     args = parser.parse_args()
 
     if args.file:
-        if args.count_words:
+        if read_file(args.file) is None:
+            print(f"Oops! File '{args.file}' not found.")
+        elif args.count_words:
             count_words(args.file)
         elif args.count_lines:
             count_lines(args.file)
@@ -51,6 +53,8 @@ def main():
             count_some_word(args.file, args.count_some_word)
         else:
             parser.print_help()
+    else:
+        parser.print_help()
 
 
 main()
