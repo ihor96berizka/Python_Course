5c48d0440d27f6f0f61096c495836f68b716f36c
LiyaNataliia <s.nataliiaa@gmail.com>
2024-01-03 12:44:40 +0200

    Changed task31

--- lab_1/task31.py
+++ lab_1/task31.py
@@ -1,9 +1,10 @@
 import random
+
 N = int(input('What`s number? '))
 
 with open('numbers.txt', 'w') as file:
     i = 0
     while i < N:
-        random_number = random.randint(1, 100)
+        random_number = random.randint(0, 100)
         file.write(f'{random_number}\n')
         i += 1
