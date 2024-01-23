034b8e06f846009ef00c57f4d50bca544a65c6a0
LiyaNataliia <s.nataliiaa@gmail.com>
2024-01-23 22:49:07 +0200

    Changed

--- lab_1/task13.py
+++ lab_1/task13.py
@@ -1,12 +1,12 @@
 N = int(input("What`s N?"))
 
 
-def is_prime(N):
-    if N <= 1:
+def is_prime(n):
+    if n <= 1:
         return False  # 1 and negative numbers are not prime
 
-    for i in range(2, int(N**0.5) + 1):
-        if N % i == 0:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False  # N is evenly divisible by i, so N is not prime
 
     return True  # If no i divides N, then N is prime
