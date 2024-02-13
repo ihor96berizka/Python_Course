cc551e03888b6720e426043635221d394863821a
LiyaNataliia <s.nataliiaa@gmail.com>
2024-02-13 18:48:29 +0200

    Changed

--- lab_1/task15.py
+++ lab_1/task15.py
@@ -1,6 +1,9 @@
+import math
+
+
 def f(x):
     # f = 0.5*x**3 + 0.5*x**2 - 1
-    f = (x - 0.955)**2
+    f = (x-0.955)**2 - 1
     return f
 
 
@@ -10,19 +13,19 @@ def f(x):
 def ihm(a, b, eps):
     while (b - a) / 2 > eps:
         c = (a + b) / 2
-        if f(c) == 0:
+        if math.isclose(f(c), 0, rel_tol=eps, abs_tol=eps):
             return c
         elif f(c) * f(a) < 0:
             b = c
         else:
             a = c
 
-    return round((a + b) / 2)
+    return (a + b) / 2
 
 
 def main():
-        eps = 1e-4
-        a = -5
+        eps = 1e-6
+        a = 0
         b = 5
         result = ihm(a, b, eps)
         print(result)
