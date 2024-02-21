9f0887e427c7f110e493231788f69c1939c077b1
LiyaNataliia <s.nataliiaa@gmail.com>
2024-02-21 16:51:44 +0200

    Made a standard file view

--- lab_1/task26.py
+++ lab_1/task26.py
@@ -1,13 +1,25 @@
-A = [5, 2, 4, 6, 1, 3]
-j = 2
+def data_in():
+    list_num = list(map(int, input("Enter a list of numbers: ").split()))
+    return list_num
+
 
 # insertion sort
-for j in range(1, len(A)):
-    key = A[j]
-    i = j - 1
-    while i >= 0 and A[i] > key:
-        A[i + 1] = A[i]
-        i = i - 1
-    A[i + 1] = key
+def insertion_sort(list_num):
+    for j in range(1, len(list_num)):
+        key = list_num[j]
+        i = j - 1
+        while i >= 0 and list_num[i] > key:
+            list_num[i + 1] = list_num[i]
+            i = i - 1
+        list_num[i + 1] = key
+    return list_num
 
-print(A)
+
+def main():
+    num = data_in()
+    print("Incoming data: ", num)
+    result = insertion_sort(num)
+    print("Sorted data: ", result)
+
+
+main()
